463506369bc404518c4291eec1bea688633f3693
gustave ploix <gustaveploix.pro@gmail.com>
2022-05-01 00:54:55 +0200

    created and styled the navbar, need to do the responsive

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Image from 'next/image';
 
 import styles from './navbar.module.scss';
@@ -8,6 +9,8 @@ import logo from '../../assets/images/logo.jpg';
 
 export default function Navbar() {
 
+    const [hovering, setHovering] = useState(false);
+
     return (
 
         <NavbarLayout>
@@ -15,15 +18,62 @@ export default function Navbar() {
                 <Image src={logo} alt="artistic (in)View" className={styles.logoImg} />
             </div>
 
-            <div className={styles.navLinks}>
-                <p><a href="#">ARTISTIC (IN) VIEW STUDIO</a></p>
-                <p><a href="#">ARTWORK</a></p>
-                <p><a href="#">NUEE</a></p>
-                <p><a href="#">DESIGN TEXTILE</a></p>
-                <p><a href="#">VISUAL RESEARCH</a></p>
-                <p><a href="#">ABOUT</a></p>
-                <p><a href="#">CONTACT</a></p>
-                <p><a href="#">HOME</a></p>
+            <div
+                onMouseEnter={() => setHovering(true)}
+                onMouseLeave={() => setHovering(false)}
+                className={styles.navLinks}>
+                <p
+                    style={{
+                        color: hovering ? "gray" : "black"
+                    }}
+                ><a href="#">ARTISTIC (IN) VIEW STUDIO</a></p>
+                <div className={styles.dropdown}>
+
+                    <p
+                        style={{
+                            color: hovering ? "gray" : "black"
+                        }}
+                    >
+                        <a href="#">
+                            ARTWORK
+                        </a>
+                    </p>
+                    <div
+                        className={styles.dropdownContent}>
+                        <a href="#">DOUSED SILK</a>
+                        <a href="#">MY BROKEN THINGS WITH LOVE...</a>
+                    </div>
+                </div>
+                <p
+                    style={{
+                        color: hovering ? "gray" : "black"
+                    }}
+                ><a href="#">NUEE</a></p>
+                <p
+                    style={{
+                        color: hovering ? "gray" : "black"
+                    }}
+                ><a href="#">DESIGN TEXTILE</a></p>
+                <p
+                    style={{
+                        color: hovering ? "gray" : "black"
+                    }}
+                ><a href="#">VISUAL RESEARCH</a></p>
+                <p
+                    style={{
+                        color: hovering ? "gray" : "black"
+                    }}
+                ><a href="#">ABOUT</a></p>
+                <p
+                    style={{
+                        color: hovering ? "gray" : "black"
+                    }}
+                ><a href="#">CONTACT</a></p>
+                <p
+                    style={{
+                        color: hovering ? "gray" : "black"
+                    }}
+                ><a href="#">HOME</a></p>
             </div>
         </NavbarLayout>
 
