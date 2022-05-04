6cce6beffe472e8b073aa4e6d4099b6838979b7d
gustave ploix <gustaveploix.pro@gmail.com>
2022-05-04 16:47:29 +0200

    changed the location of the nav and hamburger to the main template

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
+import { useState, useEffect } from 'react';
+
 import Head from 'next/head'
+
 import styles from '../styles/Home.module.css'
 
 import Navbar from '../components/navbar/navbar'
 import HamburgerMenu from '../components/hamburgerMenu/hamburger'
 
 export default function Home() {
+
+  
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,9 +19,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Navbar />
-      <HamburgerMenu />
-      
+    
     </div>
   )
 }
