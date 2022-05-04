50bf699509e0fd60aad18aeaef2c521e2e481dae
gustave ploix <gustaveploix.pro@gmail.com>
2022-05-04 16:46:52 +0200

    added the navbar and hamburger to the main template

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
+import { useState, useEffect } from 'react';
+
+import Navbar from '../components/navbar/navbar';
+import HamburgerMenu from '../components/hamburgerMenu/hamburger';
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  let windowWidth;
+
+  useEffect(() => {
+    windowWidth = window.innerWidth
+  }, [])
+  const [width, setWidth] = useState(windowWidth);
+  
+  
+  useEffect(() => {
+    const handleResise = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResise);
+    return () => window.removeEventListener("resize", handleResise)
+  }, [])
+
+  const breakpoint = 1120;
+
+  return (
+    <>
+    {width < breakpoint ? <HamburgerMenu /> : <Navbar />}
+
+      <Component {...pageProps}/>
+    </>
+  )
 }
 
 export default MyApp
