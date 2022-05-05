18d154b12def217fa92e57509ae0060812f989b9
gustave ploix <gustaveploix.pro@gmail.com>
2022-05-05 07:54:07 +0200

    added and styled the logo to the hamb menu

--- components/hamburgerMenu/hamburger.jsx
+++ components/hamburgerMenu/hamburger.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 
+import Image from 'next/image';
+import logo from '../../assets/images/logo.jpg';
+
 import HamburgerLayout from "./hamburgerLayout";
 
 import styles from './hamburger.module.scss';
@@ -16,16 +19,28 @@ export default function HamburgerMenu() {
 
         <HamburgerLayout >
 
-            <div
-                onClick={() => setOpenned(!openned)}
-            className={styles.hamburgerBtn}>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
+            <div className={styles.hamburgerNavbar}>
+                <div
+                    onClick={() => setOpenned(!openned)}
+                    className={styles.hamburgerBtn}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
 
+                {/* <img src="../../assets/images/logo.jpg" alt="" /> */}
+                <Image 
+
+                    width={136}
+                    height={43}
+                    objectFit="contain"
+                    src={logo} 
+                    className={styles.logo}
+                />
+
+            </div>
             <section className={styles.navigationSection}
-            
+
                 style={{
 
                     left: openned ? "0px" : "-100%"
