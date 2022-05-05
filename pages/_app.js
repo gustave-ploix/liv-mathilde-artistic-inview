e475d75e43a4bc6b3edc394548e980eac75c7cf3
gustave ploix <gustaveploix.pro@gmail.com>
2022-05-05 11:07:06 +0200

    refactored the responsive of navigation

--- pages/_app.js
+++ pages/_app.js
@@ -6,26 +6,12 @@ import HamburgerMenu from '../components/hamburgerMenu/hamburger';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  // return <Component {...pageProps} />
-  let windowWidth;
-
-  useEffect(() => {
-    windowWidth = window.innerWidth
-  }, [])
-  const [width, setWidth] = useState(windowWidth);
-  
-  
-  useEffect(() => {
-    const handleResise = () => setWidth(window.innerWidth);
-    window.addEventListener("resize", handleResise);
-    return () => window.removeEventListener("resize", handleResise)
-  }, [])
-
-  const breakpoint = 1120;
 
   return (
     <>
-    {width < breakpoint ? <HamburgerMenu /> : <Navbar />}
+
+      <Navbar />
+      <HamburgerMenu />
 
       <Component {...pageProps}/>
     </>
