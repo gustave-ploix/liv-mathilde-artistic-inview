2a5466e847ce8003e3c01502482e0308c2d52750
gustave ploix <gustaveploix.pro@gmail.com>
2022-05-06 09:29:54 +0200

    added the cartel and a function that allows the user to automaticly scroll to the bottom of the container

--- components/Herobanner/herobanner.jsx
+++ components/Herobanner/herobanner.jsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+
 import HeroLayout from "./heroLayout";
 
 import styles from './herobanner.module.scss';
@@ -6,10 +8,51 @@ import Image from 'next/image';
 import imgHeroBanner from '../../assets/images/landing page/heroBanner.jpg';
 
 export default function Herobanner() {
+
+    const imageContainer = useRef();
+
+    function handleScrollView() {
+        console.log(imageContainer.current)
+        imageContainer.current.scrollIntoView({ behavior: "smooth"})
+    }
+
     return (
 
         <HeroLayout>
-            <Image src={imgHeroBanner}/>
+            <div  className={styles.bannerContainer}>
+
+                <Image
+                    layout="fill"
+                    objectFit="cover"
+                    quality={80}
+                    src={imgHeroBanner} />
+                <div className={styles.cartel}>
+                    <h1>artistic (<i>IN</i>)View</h1>
+                    <h2>Liv Mathilde Méchin</h2>
+                    <div className={styles.separator}></div>
+                    <div className={styles.linkContainer}>
+                        <p>
+                            <a href="#">VISAL RESEARCH DESIGNER </a>
+                            -
+                            <em> COULEURS & MATIÈRE </em>
+                            -
+                            <a href="#"> DESIGN TEXTILE</a>
+                        </p>
+                        <p>
+                            <a href="#">DIRECTION ARTISTIQUE & CRÉATIVE </a>
+                            -
+                            <em> PHOTO STYLINE</em>
+                        </p>
+                    </div>
+                        <div
+                            ref={imageContainer}
+                            onClick={handleScrollView}
+                        className={styles.arrowContainer}>
+                            <div className={styles.arrowBranch}></div>
+                            <div className={styles.arrowBranch}></div>
+                        </div>
+                </div>
+            </div>
         </HeroLayout>
 
     )
