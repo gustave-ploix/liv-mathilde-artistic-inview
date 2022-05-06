3766e208bc2069db2bcb719279c5cebbeeae8559
gustave ploix <gustaveploix.pro@gmail.com>
2022-05-06 12:07:01 +0200

    created an empty componnent for the about section

---
+++ components/aboutSection/aboutSection.jsx
@@ -0,0 +1,14 @@
+import styles from './aboutSection.module.scss';
+import AboutSectionLayout from './aboutSectionLayout';
+
+export default function aboutSection() {
+
+
+    return (
+        <AboutSectionLayout>
+
+            <h1>about</h1>
+
+        </AboutSectionLayout>
+    )
+}
