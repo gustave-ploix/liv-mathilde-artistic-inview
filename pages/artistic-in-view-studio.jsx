2557dbaaf21deaa72467c2a5c2064f835fe86aaa
gustave ploix <gustaveploix.pro@gmail.com>
2022-05-10 14:40:31 +0200

    added the different images inside the page

--- pages/artistic-in-view-studio.jsx
+++ pages/artistic-in-view-studio.jsx
@@ -1,9 +1,20 @@
+import ImageWrapper from '../components/AIVS/ImageWrapper';
+
 import Footer from '../components/common/footer';
+
+import raniaImage from '../assets/images/AIVS/rania.jpg';
+import researchGoldRing from '../assets/images/AIVS/research_gold_ring.png';
+import researchYellowPull from '../assets/images/AIVS/research_yellow_pull.jpg';
+import researchYellowPull2 from '../assets/images/AIVS/research_yellow_pull_2.jpg';
+
+import styles from '../components/AIVS/ImageWrapper/imageWrapper.module.scss';
 
 export default function ArtisticInViewStudio() {
     return (
         <>
-
+            <ImageWrapper image1={researchYellowPull2} image2={null} classname={styles.firstImage} />
+            <ImageWrapper image1={researchGoldRing} image2={null} classname={styles.secondImage} />
+            <ImageWrapper image1={researchYellowPull} image2={raniaImage} classname={styles.thirdWrapper} />
 
             <Footer />
         </>
