d73a78d74b4b128cd724cc135f2a4f161e6ccb70
gustave ploix <gustaveploix.pro@gmail.com>
2022-05-10 14:45:45 +0200

    added head tag to the first and second page

--- pages/artistic-in-view-studio.jsx
+++ pages/artistic-in-view-studio.jsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import ImageWrapper from '../components/AIVS/ImageWrapper';
 
 import Footer from '../components/common/footer';
@@ -12,9 +14,13 @@ import styles from '../components/AIVS/ImageWrapper/imageWrapper.module.scss';
 export default function ArtisticInViewStudio() {
     return (
         <>
+            <Head>
+                <link rel="shortcut icon" href="favicon.jpeg" type="image/x-icon" />
+                <title>Artistic (in) view studio</title>
+            </Head>
             <ImageWrapper image1={researchYellowPull2} image2={null} classname={styles.firstImage} />
-            <ImageWrapper image1={researchGoldRing} image2={null} classname={styles.secondImage} />
-            <ImageWrapper image1={researchYellowPull} image2={raniaImage} classname={styles.thirdWrapper} />
+            {/* <ImageWrapper image1={researchGoldRing} image2={null} classname={styles.secondImage} />
+            <ImageWrapper image1={researchYellowPull} image2={raniaImage} classname={styles.thirdWrapper} /> */}
 
             <Footer />
         </>
