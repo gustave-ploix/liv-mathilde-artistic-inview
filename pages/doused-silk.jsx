b085a3ab00743ad85370d9f5498958e17a8e0356
gustave ploix <gustaveploix.pro@gmail.com>
2022-06-06 17:40:31 +0200

    created the doused silk page

---
+++ pages/doused-silk.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function DousedSilk() {
+    return (
+        <>
+            <Head>
+                <title>Doused Silk</title>
+                <link rel="shortcut icon" href="favicon.jpeg" type="image/x-icon" />
+            </Head>
+        </>
+    )
+}
