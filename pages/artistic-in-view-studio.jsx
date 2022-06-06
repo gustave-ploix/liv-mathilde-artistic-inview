9fca0059f4554a20804017a8d7ac857f2af93ce8
gustave ploix <gustaveploix.pro@gmail.com>
2022-06-06 14:39:01 +0200

    refactored the AIVS page

--- pages/artistic-in-view-studio.jsx
+++ pages/artistic-in-view-studio.jsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 
 import ImageWrapper from '../components/AIVS/ImageWrapper';
+import TextWrapper from '../components/AIVS/TextWrapper';
+import HeaderWrapper from '../components/AIVS/HeaderWrapper';
 
 import Footer from '../components/common/footer';
 
@@ -8,12 +10,9 @@ import raniaImage from '../assets/images/AIVS/rania.jpg';
 import researchGoldRing from '../assets/images/AIVS/research_gold_ring.png';
 import researchYellowPull from '../assets/images/AIVS/research_yellow_pull.jpg';
 import researchYellowPull2 from '../assets/images/AIVS/research_yellow_pull_2.jpg';
-import natureMorteOne from '../assets/images/AIVS/artistic-in-view-nature-morte-111b.jpg';
-import natureMorteTwo from '../assets/images/AIVS/artistic-in-view-nature-morte-112.jpg';
 
 
 import styles from '../components/AIVS/ImageWrapper/imageWrapper.module.scss';
-import TextWrapper from '../components/AIVS/TextWrapper';
 
 export default function ArtisticInViewStudio() {
     return (
@@ -22,13 +21,13 @@ export default function ArtisticInViewStudio() {
                 <link rel="shortcut icon" href="favicon.jpeg" type="image/x-icon" />
                 <title>Artistic (in) view studio</title>
             </Head>
-            <ImageWrapper image1={natureMorteOne} image2={natureMorteTwo} containerName={styles.headerContainer} classname={styles.imagesHeader} />
+            <HeaderWrapper />
             <TextWrapper />
             <ImageWrapper image1={researchYellowPull2} image2={null} containerName={styles.firstContainer} classname={styles.firstImage} />
             <ImageWrapper image1={researchGoldRing} image2={null} containerName={styles.secondContainer} classname={styles.secondImage} />
-         <ImageWrapper image1={researchYellowPull} image2={raniaImage} containerName={styles.thirdContainer} classname={styles.thirdWrapper} />  
+            <ImageWrapper image1={researchYellowPull} image2={raniaImage} containerName={styles.thirdContainer} classname={styles.thirdWrapper} />
 
             <Footer />
         </>
-    )   
+    )
 }
