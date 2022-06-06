f53ee13c6d82a3ff97dec0e16a8232b8dd11810b
gustave ploix <gustaveploix.pro@gmail.com>
2022-06-06 21:49:54 +0200

    modified the disposition of the images in the header

--- components/AIVS/HeaderWrapper/index.jsx
+++ components/AIVS/HeaderWrapper/index.jsx
@@ -9,8 +9,14 @@ export default function HeaderWrapper() {
     return (
         <HeaderWrapperLayout>
             <div className={styles.headerContainer}>
-                <Image className={styles.images} src={image1} alt="image of a person in a dark background with glasses"/>
-                <Image className={styles.images} src={image2} alt="image of a person in a dark background with glasses"/>
+                <Image className={styles.images} src={image1} alt="image of a person in a dark background with glasses"
+                style={{
+                    marginTop: "15px"
+                }}/>
+                <Image className={styles.images} src={image2} alt="image of a person in a dark background with glasses"
+                style={{
+                    marginTop: "15px"
+                }} />
             </div>
         </HeaderWrapperLayout>
     )
