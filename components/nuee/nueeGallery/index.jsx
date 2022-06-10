4a972363e252dad7fa75eac4e82ebb234b42647d
gustave ploix <gustaveploix.pro@gmail.com>
2022-06-10 13:25:30 +0200

    added the alternantives text in the second gallery

--- components/nuee/nueeGallery/index.jsx
+++ components/nuee/nueeGallery/index.jsx
@@ -16,11 +16,11 @@ import image27 from '../../../assets/images/nuee/27.jpeg';
 export default function NueeGallery() {
     return (
         <NueeGalleryLayout>
-            <FullPageGallery propSrc={image22} />
-            <FullPageGallery propSrc={image23} />
-            <RowGallery firstSrc={image24} secondSrc={image25} />
-            <FullPageGallery propSrc={image26} />
-            <FullPageGallery propSrc={image27} />
+            <FullPageGallery propSrc={image22} propAlt="image of the nuée's catalog" />
+            <FullPageGallery propSrc={image23} propAlt="bed covered with nuée's drapes and pillows" />
+            <RowGallery firstSrc={image24} secondSrc={image25} firstAlt="grey nuée's pillow" secondAlt="green nuée's pillow" />
+            <FullPageGallery propSrc={image26} propAlt="pink nuée's pillow" />
+            <FullPageGallery propSrc={image27} propAlt="table covered by a nuée's drape" />
         </NueeGalleryLayout>
     )
 }
