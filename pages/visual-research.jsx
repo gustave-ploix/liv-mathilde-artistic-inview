f87a302e6eeba8cf6adb62892b1b8e6dec1b56c4
gustave ploix <gustaveploix.pro@gmail.com>
2022-06-10 16:11:36 +0200

    created a boilerplate for the visualResearch page

---
+++ pages/visual-research.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+import Footer from '../components/common/footer';
+
+export default function VisualResearch() {
+    return (
+        <>
+            <Head>
+                <title>Visual research</title>
+                <link rel="shortcut icon" href="favicon.jpeg" type="image/x-icon" />
+                <meta name="description" content="Page presenting the visual researchs of Liv Mathilde Mechin" />
+            </Head>
+
+
+            <Footer />
+        </>
+    )
+}
