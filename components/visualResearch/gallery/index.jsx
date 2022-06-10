9b1ef78b0500a5007a288a42eb419259e65f65dd
gustave ploix <gustaveploix.pro@gmail.com>
2022-06-10 23:41:03 +0200

    finished the styling of the visual research page

--- components/visualResearch/gallery/index.jsx
+++ components/visualResearch/gallery/index.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 
 import FullPageGallery from '../../common/fullPageGallery';
 import RowGallery from '../../common/rowGallery';
+import TripleRow from '../../common/tripeRow';
 
 import image01 from '../../../assets/images/visualResearch/1.jpg';
 import image02 from '../../../assets/images/visualResearch/2.jpg';
@@ -22,6 +23,22 @@ import image14 from '../../../assets/images/visualResearch/14.jpg';
 import image15 from '../../../assets/images/visualResearch/15.jpeg';
 import image16 from '../../../assets/images/visualResearch/16.jpg';
 import image17 from '../../../assets/images/visualResearch/17.jpeg';
+
+import image18 from '../../../assets/images/visualResearch/18.jpeg';
+import image19 from '../../../assets/images/visualResearch/19.jpg';
+import image20 from '../../../assets/images/visualResearch/20.jpg';
+import image21 from '../../../assets/images/visualResearch/21.jpg';
+import image22 from '../../../assets/images/visualResearch/22.jpg';
+import image23 from '../../../assets/images/visualResearch/23.jpg';
+import image24 from '../../../assets/images/visualResearch/24.jpg';
+import image25 from '../../../assets/images/visualResearch/25.jpg';
+import image26 from '../../../assets/images/visualResearch/26.jpg';
+import image27 from '../../../assets/images/visualResearch/27.jpg';
+import image28 from '../../../assets/images/visualResearch/28.jpg';
+import image29 from '../../../assets/images/visualResearch/29.jpg';
+import image30 from '../../../assets/images/visualResearch/30.jpg';
+import image31 from '../../../assets/images/visualResearch/31.jpg';
+import image32 from '../../../assets/images/visualResearch/32.jpg';
 
 
 import styles from './gallery.module.scss';
@@ -45,6 +62,22 @@ export default function Gallery() {
             <RowGallery firstSrc={image14} firstAlt="visual research" secondSrc={image15} secondAlt="visual research" />
             <RowGallery firstSrc={image16} firstAlt="visual research" secondSrc={image17} secondAlt="visual research" />
 
+            <div className={styles.secondTitle}>
+                <div className={styles.line}></div>
+                <p className={styles.title}>Recherches de couleurs, harmonies colorées</p>
+            </div>
+
+            <FullPageGallery propSrc={image18}/>
+            <FullPageGallery propSrc={image19}/>
+            <FullPageGallery propSrc={image20}/>
+            <FullPageGallery propSrc={image21}/>
+            <FullPageGallery propSrc={image22}/>
+            <TripleRow firstSrc={image23} secondSrc={image24} thirdSrc={image25}/>
+            <RowGallery firstSrc={image26} secondSrc={image27} firstAlt="visual research" secondAlt="visual research" />
+            <FullPageGallery propSrc={image28} propAlt="visual research" />
+            <RowGallery firstSrc={image29} secondSrc={image30} firstAlt="visual research" secondAlt="visual research" />
+            <RowGallery firstSrc={image31} secondSrc={image32} firstAlt="visual research" secondAlt="visual research" />
+                
         </GalleryLayout>
     )
 }
--- pages/visual-research.jsx
+++ pages/visual-research.jsx
@@ -1,5 +1,13 @@
 import Head from 'next/head';
 
+import Header from '../components/common/header/header';
+import headerImage from '../assets/images/visualResearch/header.jpg';
+
+import Title from '../components/visualResearch/title';
+import Gallery from '../components/visualResearch/gallery';
+
+import styles from '../components/common/header/header.module.scss';
+
 import Footer from '../components/common/footer';
 
 export default function VisualResearch() {
@@ -11,6 +19,10 @@ export default function VisualResearch() {
                 <meta name="description" content="Page presenting the visual researchs of Liv Mathilde Mechin" />
             </Head>
 
+
+            <Header headerSrc={headerImage} propClassname={styles.smallerHeader} headerAlt="two research made by liv mathilde mechin." />
+            <Title />
+            <Gallery />
 
             <Footer />
         </>
