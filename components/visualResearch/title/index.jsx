52c3251127ca6bae5b043e8a8cad801a96fda13b
gustave ploix <gustaveploix.pro@gmail.com>
2022-06-10 22:00:44 +0200

    finished the responsive of the title

--- components/visualResearch/title/index.jsx
+++ components/visualResearch/title/index.jsx
@@ -6,10 +6,10 @@ export default function Title() {
     return (
         <TitleLayout>
             <h1>VISUAL RESEARCH</h1>
-            <div className={styles.plainTextContainers}>
+            <div className={styles.plainTextContainer}>
                 <p className={styles.plainText}>
                     Dessins, maquettes, pliages, assemblages de matières, associations colorées,
-                    décompositions, superpositions, jouer et déjouer les contours d&apos;une forme.....
+                    décompositions, superpositions, jouer et déjouer les contours d&apos;une forme..... <br />
                     des éléments librement composés pour suggérer des histoires
                 </p>
                 <p className={styles.plainText}>
@@ -21,7 +21,7 @@ export default function Title() {
                     dessin textile, dossiers de style, gammes de couleurs,
                 </p>
             </div>
-            <p className={styles.titleFooter}><span>&#123;</span> Intention Artistique, Dessin, Recherche Visuel <span>&#125;</span> </p>
+            <p className={styles.titleFooter}><span>&#123;</span> Intention Artistique, Dessin, Recherche Visuelle <span>&#125;</span> </p>
         </TitleLayout>
     )
 } 
