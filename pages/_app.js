c4240e3754af37a8c56748c31c8c52b692048c12
gustave ploix <gustaveploix.pro@gmail.com>
2022-06-24 13:28:33 +0200

    added the two different fonts to the project

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import Navbar from '../components/common/navbar/navbar';
 import HamburgerMenu from '../components/common/hamburgerMenu/hamburger';
 
@@ -7,11 +9,18 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        {/* supreme font */}
+        <link href="https://api.fontshare.com/v2/css?f[]=supreme@1&display=swap" rel="stylesheet" /> 
+
+        {/* general sans font */}
+        <link href="https://api.fontshare.com/v2/css?f[]=general-sans@1&display=swap" rel="stylesheet"></link>
+      </Head>
 
       <Navbar />
       <HamburgerMenu />
 
-      <Component {...pageProps}/>
+      <Component {...pageProps} />
     </>
   )
 }
