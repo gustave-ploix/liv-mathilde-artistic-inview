883c36b6af52e59f6ff7750647faf317bf6825c3
gustave ploix <gustaveploix.pro@gmail.com>
2022-06-25 15:52:39 +0200

    added the meta viewport to the project

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+
         {/* supreme font */}
         <link href="https://api.fontshare.com/v2/css?f[]=supreme@1&display=swap" rel="stylesheet" /> 
 
