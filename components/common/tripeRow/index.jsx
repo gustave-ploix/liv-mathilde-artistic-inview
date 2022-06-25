354f8a5e4509e2c47634dfb7f8fe3b15ef9a35e5
gustave ploix <gustaveploix.pro@gmail.com>
2022-06-25 11:35:29 +0200

    modified the triple row to be able to specify or not alts

--- components/common/tripeRow/index.jsx
+++ components/common/tripeRow/index.jsx
@@ -7,18 +7,21 @@ import TripleRowLayout from './tripleRowLayout';
 export default function TripleRow({
     firstSrc,
     secondSrc,
-    thirdSrc
+    thirdSrc,
+    firstAlt,
+    secondAlt,
+    thirdAlt
 }) {
     return (
         <TripleRowLayout>
             <div className={styles.firstImage}>
-                <Image placeholder='blur' src={firstSrc} alt="visual research" layout="fill" objectFit='cover' quality={100} />
+                <Image placeholder='blur' src={firstSrc} alt={firstAlt ? firstAlt : "visual research"} layout="fill" objectFit='cover' quality={100} />
             </div>
             <div className={styles.secondImage}>
-                <Image placeholder='blur' src={secondSrc} alt="visual research" layout="fill" objectFit='cover' quality={100} />
+                <Image placeholder='blur' src={secondSrc} alt={secondAlt ? secondAlt : "visual research"} layout="fill" objectFit='cover' quality={100} />
             </div>
             <div className={styles.thirdImage}>
-                <Image placeholder='blur' src={thirdSrc} alt="visual research" layout="fill" objectFit='cover' quality={100} />
+                <Image placeholder='blur' src={thirdSrc} alt={thirdAlt ? thirdAlt : "visual research"} layout="fill" objectFit='cover' quality={100} />
             </div>
         </TripleRowLayout>
     )
